2e112ad653de5cee44e34fd1a4fed4e6fefe9084
Ahsath <ahsath@gmail.com>
2020-06-08 16:16:43 -0500

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <app-shell>
-    <app-banner title="Venta y talla de rocas preciosas, semipreciosas y fósiles">
+    <app-banner title="Venta y reparación de celulares, tablet y computadoras.">
       <template #banner-img>
         <g-image
           src="@/assets/img/banner-img.jpg"
-          alt="Un hombre puliendo una gema"
+          alt="Gama de celulares"
           class="absolute top-0 left-0 h-full object-cover is-block"
         />
       </template>
